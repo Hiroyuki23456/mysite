1b8067f88b808fad6c5f1517056c330968cdabb5
Hiro牧 <hiroyukimakino9@gmail.com>
2023-06-01 01:04:22 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+JavaScript における数値の表現
+*/
+// console.log(100);
+// console.log(-10);
+// console.log(2.5);
+
+//大きな表現、小さな数値の表現
+console.log(1.2e4); //e とは10の4乗
+console.log(1.2e-4);// -eとは10の-4乗
